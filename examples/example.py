a04da2a540a765e6d39f397b755be9ba860abcaf
CyanBrown <cyanbrown29@gmail.com>
2020-10-02 15:08:56 -0500

    added more examples
    Signed-off-by: CyanBrown <cyanbrown29@gmail.com>

--- examples/example.py
+++ examples/example.py
@@ -2,13 +2,14 @@ from src.physics import physics
 
 obj = physics(vector=[60, 60])
 print(obj.x_values, obj.y_values)
+obj.proj.graph('all', save=True)
 obj.proj.graph('ty')
 obj.proj.graph('tx')
 obj.proj.graph('xy')
 
 """
 output
-{'X0': 0, 'X': 3.42, 'A': 0, 'V0': 30.0, 'V': 30.0, 'T': 0.114} {'V': -53.082, 'V0': 51.962, 'X': 0, 'X0': 6, 'T': 0.114, 'A': -9.8}
+{'X0': 0, 'X': 3.42, 'A': 0, 'V0': 30.0, 'V': 30.0, 'T': 0.114} {'V': -53.082, 'V0': 51.962, 'X': 0, 'X0': 6, 'T': 0.114,'A': -9.8}
 and the images in the folder
 """
 
